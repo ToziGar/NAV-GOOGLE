5e29414b711d24eede688c830eae74485adf6e50
Nicholas <nicholasganshyan@gmail.com>
2018-12-22 14:57:49 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Android-GmailNavigationDrawer
+_An example illustrating how to implement a Navigation Drawer similar to the one in Google's Gmail App in Android_
+
+Google's Gmail App mostly follows the standard navigation drawer you get with the standard material design library. However, there are several  features that sets it apart, including a drop down arrow in the drawer's header. When clicked, another drawer menu is shown. This article will show you how to implement those features.
+
+If you are not sure of how to implement a standard navigation drawer, you can click these links:
+1. [Create a navigation drawer](https://developer.android.com/training/implementing-navigation/nav-drawer) - Official documentation on how to create a navigation drawer
+2. [wRorsjakz/Android-NavigationDrawer](https://github.com/wRorsjakz/Android-NavigationDrawer) - My other Github repository on how to implement a navigation drawer
+
+<br>
+<img src="https://img.shields.io/badge/minSdkVersion-21-red.svg?style=true" alt="minSdkVersion 21" data-canonical-src="https://img.shields.io/badge/minSdkVersion-24-red.svg?style=true" style="max-width:100%;">
+<img src=https://img.shields.io/badge/compileSdkVersion-28-brightgreen.svg alt="compileSdkVersion 28" data-canonical-src="https://img.shields.io/badge/compileSdkVersion-27-yellow.svg?style=true" style="max-width:100%;">
+
+## Navigation Drawer In Action
+
+## Tutorial
---
+++ app/src/main/java/com/example/user/gmailappnavigationdrawerclone/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.user.gmailappnavigationdrawerclone;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ImageView imageViewArrow;
+    private int x;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeViews();
+        handleHeaderToggle();
+        initializeDefault();
+    }
+
+    private void initializeViews() {
+        drawerLayout = findViewById(R.id.main_drawerlayout_id);
+        navigationView = findViewById(R.id.main_navigationview_id);
+        toolbar = findViewById(R.id.main_toolbar_id);
+
+        setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+        toggleNavigationDrawer();
+    }
+
+    /**
+     * Display the default menu item when app is first initialized
+     */
+    private void initializeDefault() {
+        navigationView.setCheckedItem(R.id.menu_primary);
+    }
+
+    /**
+     * Handle what happens when the toggle arrow is pressed
+     */
+    private void handleHeaderToggle() {
+        x = 0;
+
+        View header = navigationView.getHeaderView(0);
+        imageViewArrow = header.findViewById(R.id.nav_header_arrow);
+
+        imageViewArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (x == 0) {
+                    // Change the arrow
+                    imageViewArrow.setImageResource(R.drawable.ic_arrow_drop_up_white_24dp);
+                    // Remove current menu
+                    navigationView.getMenu().clear();
+                    // Inflate & Display the other menu
+                    navigationView.inflateMenu(R.menu.nav_menu_two);
+                } else if (x % 2 == 0) {
+                    imageViewArrow.setImageResource(R.drawable.ic_arrow_drop_up_white_24dp);
+                    navigationView.getMenu().clear();
+                    navigationView.inflateMenu(R.menu.nav_menu_two);
+                } else {
+                    imageViewArrow.setImageResource(R.drawable.ic_arrow_drop_down_white_24dp);
+                    navigationView.getMenu().clear();
+                    navigationView.inflateMenu(R.menu.nav_menu_one);
+                }
+                x += 1;
+            }
+        });
+    }
+
+    /**
+     * Creates an instance of the ActionBarDrawerToggle class:
+     * 1) Handles opening and closing the navigation drawer
+     * 2) Creates a hamburger icon in the toolbar
+     * 3) Attaches listener to open/close drawer on icon clicked and rotates the icon
+     */
+    private void toggleNavigationDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.menu_primary:
+                Toast.makeText(this, "Primary item pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_social:
+                Toast.makeText(this, "Social item pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_promotions:
+                Toast.makeText(this, "Promotions item pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_menu_addaccount:
+                Toast.makeText(this, "Add account item pressed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_menu_manageaccounts:
+                Toast.makeText(this, "Manage accounts item pressed", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
