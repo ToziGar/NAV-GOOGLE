29bef23c39fa6d69acb90bde3af24bae43b0c9c4
Nicholas <nicholasganshyan@gmail.com>
2018-12-28 23:40:39 +0800

    Handle screen rotation & nav drawer menu toggle

--- app/src/main/java/com/example/user/gmailappnavigationdrawerclone/MainActivity.java
+++ app/src/main/java/com/example/user/gmailappnavigationdrawerclone/MainActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +22,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView navigationView;
     private Toolbar toolbar;
     private ImageView imageViewArrow;
-    private int x;
+    private FrameLayout layoutContainer;
+
+    private int currentItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,59 +32,58 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         initializeViews();
         handleHeaderToggle();
-        initializeDefault();
+        initializeDefault(savedInstanceState);
     }
 
     private void initializeViews() {
         drawerLayout = findViewById(R.id.main_drawerlayout_id);
         navigationView = findViewById(R.id.main_navigationview_id);
         toolbar = findViewById(R.id.main_toolbar_id);
+        layoutContainer = findViewById(R.id.main_framelayout_id);
 
         setSupportActionBar(toolbar);
         navigationView.setNavigationItemSelectedListener(this);
         toggleNavigationDrawer();
 
         // Set dummy data of 5 unread messages in menu_primary menu item.
-        displayCounter(R.id.menu_primary,5);
-        displayCounter(R.id.menu_social,2);
+        displayCounter(R.id.menu_primary, 5);
+        displayCounter(R.id.menu_social, 2);
     }
 
     /**
      * Display the default menu item when app is first initialized
      */
-    private void initializeDefault() {
-        navigationView.setCheckedItem(R.id.menu_primary);
+    private void initializeDefault(Bundle bundle) {
+        if(bundle == null){
+            navigationView.setCheckedItem(R.id.menu_primary);
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout_id,
+                    new PrimaryFragment()).commit();
+            currentItem = R.id.menu_primary;
+        }
     }
 
     /**
      * Handle what happens when the toggle arrow is pressed
      */
     private void handleHeaderToggle() {
-        x = 0;
-
         View header = navigationView.getHeaderView(0);
         imageViewArrow = header.findViewById(R.id.nav_header_arrow);
 
         imageViewArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (x == 0) {
-                    // Change the arrow
-                    imageViewArrow.setImageResource(R.drawable.ic_arrow_drop_up_white_24dp);
-                    // Remove current menu
-                    navigationView.getMenu().clear();
-                    // Inflate & Display the other menu
-                    navigationView.inflateMenu(R.menu.nav_menu_two);
-                } else if (x % 2 == 0) {
-                    imageViewArrow.setImageResource(R.drawable.ic_arrow_drop_up_white_24dp);
-                    navigationView.getMenu().clear();
-                    navigationView.inflateMenu(R.menu.nav_menu_two);
-                } else {
-                    imageViewArrow.setImageResource(R.drawable.ic_arrow_drop_down_white_24dp);
-                    navigationView.getMenu().clear();
-                    navigationView.inflateMenu(R.menu.nav_menu_one);
-                }
-                x += 1;
+               if(!v.isSelected()){
+                   currentItem = navigationView.getCheckedItem().getItemId();
+                   navigationView.getMenu().clear();
+                   navigationView.inflateMenu(R.menu.nav_menu_two);
+                   v.setSelected(true);
+               }
+               else{
+                   navigationView.getMenu().clear();
+                   navigationView.inflateMenu(R.menu.nav_menu_one);
+                   navigationView.setCheckedItem(currentItem);
+                   v.setSelected(false);
+               }
             }
         });
     }
@@ -100,14 +103,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     /**
      * Sets the counter for MenuItem with id of menuItemId with int count.
+     *
      * @param menuItemId
      * @param count
      */
-    private void displayCounter(int menuItemId, int count){
+    private void displayCounter(int menuItemId, int count) {
         int noOfItems = navigationView.getMenu().size();
-        for (int i=0; i < noOfItems; i++){
+        for (int i = 0; i < noOfItems; i++) {
             MenuItem item = navigationView.getMenu().getItem(i);
-            if (item.getItemId() == menuItemId){
+            if (item.getItemId() == menuItemId) {
                 TextView counter = (TextView) item.getActionView();
                 counter.setText(Integer.toString(count));
             }
@@ -116,24 +120,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.menu_primary:
-                Toast.makeText(this, "Primary item pressed", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.menu_social:
-                Toast.makeText(this, "Social item pressed", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.menu_promotions:
-                Toast.makeText(this, "Promotions item pressed", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_menu_addaccount:
-                Toast.makeText(this, "Add account item pressed", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_menu_manageaccounts:
-                Toast.makeText(this, "Manage accounts item pressed", Toast.LENGTH_SHORT).show();
-                break;
+        if (menuItem.getItemId() == navigationView.getCheckedItem().getItemId()) {
+            // Do nothing so that the fragment is not re-initialised
+            Log.d("menu_primary", "onNavigationItemSelected: Same menuitem") ;
+        } else {
+            switch (menuItem.getItemId()) {
+                case R.id.menu_primary:
+                    Log.d("menu_primary", "onNavigationItemSelected:menu_primary ");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout_id,
+                            new PrimaryFragment()).commit();
+                    break;
+                case R.id.menu_social:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout_id,
+                            new SocialFragment()).commit();
+                    break;
+                case R.id.menu_promotions:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout_id,
+                            new PromotionsFragment()).commit();
+                    break;
+                case R.id.nav_menu_addaccount:
+                    Toast.makeText(this, "Add accounts item pressed", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.nav_menu_manageaccounts:
+                    Toast.makeText(this, "Manage accounts item pressed", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
         }
-        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
@@ -145,4 +158,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
+
 }
